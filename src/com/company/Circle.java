a52b8ab70c3e1f5fd23003f803d7964cfd5ac65b
vitaliks007 <45079744+vitaliks007@users.noreply.github.com>
2021-09-11 09:25:11 +0300

    Update Circle.java

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -24,10 +24,12 @@ public class Circle extends Shape{
         this.radius = radius;
     }
 
+    @Override
     public double getArea(){
         return Math.pow(radius, 2) * Math.PI;
     }
 
+    @Override
     public double getPerimeter(){
         return 2 * radius * Math.PI;
     }
